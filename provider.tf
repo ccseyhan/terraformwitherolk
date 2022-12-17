1c4624e0c1df7ce429972b3038324b614c4a92ae
ccseyhan <cemalcan.seyhan@deop.ca>
2022-12-17 13:45:12 -0300

    provider backend added

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "3.36.0"
     }
   }
+  backend "remote" {
+    organization = "ccseyhanorg"
+    workspaces {
+      name = "terraformwitherolk"
+    }
+    
+  }
 }
 
 provider "azurerm" {
